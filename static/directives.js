4d5b3066ab45f17b3633d22c67ef05291c9bb519
muthukumarcse <muthukumar.paramasivam@gmail.com>
2015-11-13 23:08:42 +0530

    custom directives sample

---
+++ static/directives.js
@@ -0,0 +1,27 @@
+
+// Create a new angularjs module.
+var searchApp = angular.module('searchapp', []);
+
+
+// Search data which will be displayed in the UI
+var model = {
+
+	data : [{ sno:1, name:'Muthu', email:'muthu@gmail.com', phone:'9659003535', companyId:'AB-1'},{ sno:2, name:'Kumar', email:'kumar@gmail.com', phone:'879839782', companyId:'AB-1'},{ sno:3, name:'Sam', email:'sam@gmail.com', phone:'987879678', companyId:'AB-1'},{ sno:4, name:'Ram', email:'ram@gmail.com', phone:'9659003535', companyId:'AB-1'},{ sno:5, name:'Arun', email:'arun@gmail.com', phone:'345433223', companyId:'AB-1'}]
+};
+
+// Register a new controller to the searchapp
+searchApp.controller('searchController', ['$scope', function($scope) {
+	$scope.users = model;
+}])
+
+// Creating custom directive.
+searchApp.directive('searchData', function()
+{
+	return {
+	  restrict: 'A',  // A -- Only matches attribute name.
+	  templateUrl: 'data.html' 
+};
+});
+
+
+
